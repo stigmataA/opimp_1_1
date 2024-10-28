238a1f1774318633e744fdb2eb928ab265a73657
stigmataA <r.muhutdinov@gmail.com>
2024-10-28 17:16:41 +0300

    Add files via upload

--- olimp_1_1.py
+++ olimp_1_1.py
@@ -1,18 +1,18 @@
 import math
 
-K_str, M_str, T_str, X_str = input(f"Введите K, M, T, X:").split()
+K_str, M_str, T_str, X_str = input(f"Введите K, M, T, X: ").split()
 K = int(K_str)
 M = int(M_str)
 T = int(T_str)
 X = int(X_str)
 
-sevenK_minus_T = 7 * K - T
+weekK_minus_T = 7 * K - T
 
 found = False
 N = -1
 
-if sevenK_minus_T > 0:
-    q_min = max(0, (M - X - 7 * K + sevenK_minus_T - 1) // sevenK_minus_T)
+if weekK_minus_T > 0:
+    q_min = max(0, (M - X - 7 * K + weekK_minus_T - 1) // weekK_minus_T)
     for q in range(q_min, q_min + 100):  # Limit the range to prevent infinite loop
         A_max = X + q * (7 * K - T) + 7 * K
         if A_max < M:
@@ -27,7 +27,7 @@ if sevenK_minus_T > 0:
             break
     if not found:
         N = -1
-elif sevenK_minus_T == 0:
+elif weekK_minus_T == 0:
     RHS = M - X
     if RHS <= 7 * K:
         r_min = (RHS + K - 1) // K - 1
@@ -37,7 +37,7 @@ elif sevenK_minus_T == 0:
             N = -1
     else:
         N = -1
-else:  # sevenK_minus_T < 0
+else:
     A_max = X + 7 * K
     if A_max < M:
         N = -1
